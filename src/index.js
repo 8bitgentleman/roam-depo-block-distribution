d1f2aafa2f9fac43129ef0168963ea61434cbd23
Matt Vogel <mainmoniker@googlemail.com>
2024-08-22 15:14:35 -0400

    init

--- src/index.js
+++ src/index.js
@@ -1,42 +1,35 @@
-const panelConfig = {
-  tabTitle: "Test Ext 1",
-  settings: [
-      {id:          "button-setting",
-       name:        "Button test",
-       description: "tests the button",
-       action:      {type:    "button",
-                     onClick: (evt) => { console.log("Button clicked!"); },
-                     content: "Button"}},
-      {id:          "switch-setting",
-       name:        "Switch Test",
-       description: "Test switch component",
-       action:      {type:     "switch",
-                     onChange: (evt) => { console.log("Switch!", evt); }}},
-      {id:     "input-setting",
-       name:   "Input test",
-       action: {type:        "input",
-                placeholder: "placeholder",
-                onChange:    (evt) => { console.log("Input Changed!", evt); }}},
-      {id:     "select-setting",
-       name:   "Select test",
-       action: {type:     "select",
-                items:    ["one", "two", "three"],
-                onChange: (evt) => { console.log("Select Changed!", evt); }}}
-  ]
-};
+import pkg from '../package.json';
+import IntervalSettings from './components/BlockDistributionSettings';
 
 async function onload({extensionAPI}) {
   // set defaults if they dont' exist
-  if (!extensionAPI.settings.get('data')) {
-      await extensionAPI.settings.set('data', "01");
-  }
-  extensionAPI.settings.panel.create(panelConfig);
+  const wrappedIntervalConfig = () => IntervalSettings({ extensionAPI })
 
-  console.log("load example plugin");
+  extensionAPI.settings.panel.create({
+    tabTitle: `${pkg.name}`,
+    settings: [
+        {id:          "button-setting",
+         name:        "Button test",
+         description: "tests the button",
+         action:      {type:    "button",
+                       onClick: (evt) => { console.log("Button clicked!"); },
+                       content: "Button"}},
+                       {
+                        id: "interval-settings",
+                        name: "Contact Frequency Intervals",
+                        description:
+                            "Set custom contact frequency durations. See the README for more info.",
+                        className: "crm-reminders-interval-setting",
+                        action: { type: "reactComponent", component: wrappedIntervalConfig },
+                    },           
+    ]
+  });
+
+  console.log(`Loaded ${pkg.name} v${pkg.version}`);
 }
 
 function onunload() {
-  console.log("unload example plugin");
+  console.log(`Unloaded ${pkg.name}`);
 }
 
 export default {
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,36 @@
 module.exports = {
     externals: {
         react: "React",
-        "chrono-node": "ChronoNode"
+        "chrono-node": "ChronoNode",
     },
     externalsType: "window",
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        filename: 'extension.js',
+        filename: "extension.js",
         path: __dirname,
         library: {
             type: "module",
-        }
+        },
     },
     experiments: {
         outputModule: true,
     },
+    resolve: {
+        extensions: [".js", ".jsx"], // Add other extensions if needed
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-react"],
+                    },
+                },
+            },
+        ],
+    },
     mode: "production",
-};
+}
